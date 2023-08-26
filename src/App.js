b6d60d687bb74e19c1e83ef43b36832ea12a31a3
Austin Amaral <austin.amaral3@gmail.com>
2023-08-26 13:33:14 -0700

    working on data, and built dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import GifList from './components/GifList'
+import data from './data/gifs'
 
-function App() {
+function App(props) {
+  const gifs = data;
+  const loading = false;
+  const error = "";
+  
   return (
     <div className="App">
-      Async Redux Project
+      <h1> Find Gifs Here:</h1>
+
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+
+      {
+        loading ? <h3> Looking for Gif...</h3> : <GifList gifs={gifs} />
+      }
     </div>
   );
 }
